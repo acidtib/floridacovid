366b17b1b9570a79c05321a8462f40ffb684bd9a
arubinofaux <arubinofaux@gmail.com>
2020-03-09 20:25:50 -0400

    added api

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra"
+gem "sinatra-contrib"
 gem "activerecord"
 gem "sinatra-activerecord"
 gem "rake"
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/namespace'
 require 'rack/ssl-enforcer'
 
 use Rack::SslEnforcer if production?
@@ -9,4 +10,34 @@ Dir["./models/*.rb"].each { |file| require file }
 get '/' do
   @stats = State.find_by_slug("florida").stats.last
   erb :home
+end
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/cases' do
+    stats = State.find_by_slug("florida").stats.last
+    
+    {
+      "cases": {
+        residents: stats.positive_residents,
+        repatriated: stats.cases_repatriated,
+        non_residents: stats.non_residents,
+      },
+      "deaths": {
+        residents: stats.deaths
+      },
+      "results": {
+        negative: stats.results_negative,
+        pending: stats.results_pending
+      },
+      "monitored": {
+        currently: stats.being_monitored,
+        total: stats.total_monitored
+      },
+      last_update: stats.last_update
+    }.to_json
+  end
 end
