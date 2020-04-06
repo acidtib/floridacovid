315aa248ca734999e2ec3e62dba1070d66d7affd
Dan Vera <arubinofaux@gmail.com>
2020-04-05 21:42:36 -0400

    get states stats

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,4 @@
 class State < ApplicationRecord
   belongs_to :country
+  has_many :state_stats
 end
--- app/models/state_stat.rb
+++ app/models/state_stat.rb
@@ -1,3 +1,7 @@
 class StateStat < ApplicationRecord
   belongs_to :state
+
+  def self.today
+    where(created_at: Time.zone.today.all_day)
+  end
 end
