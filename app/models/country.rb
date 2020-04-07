2b3a99d15cf265eb99b5392136161821edb30164
Dan Vera <arubinofaux@gmail.com>
2020-04-07 14:17:08 -0400

    wip

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ApplicationRecord
   has_many :country_stats
+  has_many :states
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+country = Country.create(
+  name: "US",
+  slug: "us",
+  lat: "40", 
+  long: "-100"
+)
+
+state = country.states.create(
+  name: "Florida",
+  slug: "florida",
+  lat: "27.766279", 
+  long: "-81.686783"
+)
+
+state.state_stats.create(
+  positive_residents: 14065,
+  positive_non_residents: 439,
+  deaths: 283,
+  results_total: 138618,
+  results_negative: 122792,
+  recovered: 0,
+  being_monitored: 1777
+)
