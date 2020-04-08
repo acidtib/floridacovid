79b6f662cfa75a64289f6bded51fa9893e8011d5
arubinofaux <arubinofaux@gmail.com>
2020-04-08 01:23:01 -0400

    wip home

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,22 @@
 class PageController < ApplicationController
   def index
-    @state = State.find_by_slug("florida")
+    @state = State.includes(:state_stats).find_by_slug("florida")
     @state_stats = @state.state_stats.last
     @ages = @state.age_stats.last
+    
+    @countries = Country.includes(:country_stats).all
+    @co_stats = @countries.find_by_slug("us").country_stats.last
+
+    @earth_confirmed = 0
+    @earth_recovered = 0
+    @earth_deaths = 0
+  
+    @countries.each do |co|
+      stat = co.country_stats.last
+
+      @earth_confirmed += stat.confirmed
+      @earth_recovered += stat.recovered
+      @earth_deaths += stat.deaths
+    end
   end
 end
