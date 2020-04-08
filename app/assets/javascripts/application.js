7f8b9a4f19469a25de5bcce66d52311e554188cf
arubinofaux <arubinofaux@gmail.com>
2020-04-08 14:27:56 -0400

    api doc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,4 +26,11 @@ $(document).on("turbolinks:load", function() {
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
+
+  $('.scroll_to').click(function (e) {
+    var jump = $(this).attr('href');
+    var new_position = $(jump).offset();
+    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
+    e.preventDefault();
+  });
 });
