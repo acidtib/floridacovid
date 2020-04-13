1ae89d43e68f7ecbd859ba31ad47fbd4a1e9cc0d
arubinofaux <arubinofaux@gmail.com>
2020-04-13 16:08:04 -0400

    added new case to chart

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -9,6 +9,7 @@ module Api
         date_to = stats.last.created_at.to_date
         date_range = date_from..date_to
         total_per_day = []
+        new_cases = []
         residents = []
         non_residents = []
 
@@ -16,7 +17,13 @@ module Api
           stat = state.state_stats.where(created_at: d.midnight..d.end_of_day).last
           
           if stat
+            stat_yesterday = state.state_stats.where(created_at: (stat.created_at - 24.hours).all_day).last
+            if stat_yesterday.blank?
+              stat_yesterday = stat
+            end
+
             total_per_day.push([ d, (stat.positive_residents + stat.positive_non_residents) ])
+            new_cases.push([ d, ((stat.positive_residents + stat.positive_non_residents) - (stat_yesterday.positive_residents + stat_yesterday.positive_non_residents))])
             residents.push([ d, stat.positive_residents ])
             non_residents.push([ d, stat.positive_non_residents ])
           end
@@ -24,6 +31,7 @@ module Api
 
         result = [
           {name: "Total Cases Per Day", data: total_per_day, color: "#e64c00"},
+          {name: "New Cases", data: new_cases, color: "#f00"},
           {name: "Residents", data: residents, color: "#0084a8"},
           {name: "Non-Residents", data: non_residents, color: "#fed60a"}
         ]
