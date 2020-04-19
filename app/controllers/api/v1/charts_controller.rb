7d2b66a7a73f457987b7556966abd029d3d465c8
arubinofaux <arubinofaux@gmail.com>
2020-04-19 15:44:15 -0400

    chart only show new cases per day

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -8,10 +8,7 @@ module Api
         date_from = stats.first.created_at.to_date
         date_to = stats.last.created_at.to_date
         date_range = date_from..date_to
-        total_per_day = []
         new_cases = []
-        residents = []
-        non_residents = []
 
         date_range.map {|d| Date.new(d.year, d.month, d.day) }.uniq.each do |d|
           stat = state.state_stats.where(created_at: d.midnight..d.end_of_day).last
@@ -22,18 +19,12 @@ module Api
               stat_yesterday = stat
             end
 
-            total_per_day.push([ d, (stat.positive_residents + stat.positive_non_residents) ])
             new_cases.push([ d, ((stat.positive_residents + stat.positive_non_residents) - (stat_yesterday.positive_residents + stat_yesterday.positive_non_residents))])
-            residents.push([ d, stat.positive_residents ])
-            non_residents.push([ d, stat.positive_non_residents ])
           end
         end
 
         result = [
-          {name: "Total Cases Per Day", data: total_per_day, color: "#e64c00"},
-          {name: "New Cases", data: new_cases, color: "#f00"},
-          {name: "Residents", data: residents, color: "#0084a8"},
-          {name: "Non-Residents", data: non_residents, color: "#fed60a"}
+          {name: "New Cases", data: new_cases, color: "#f00"}
         ]
         
         render json: result, status: 200
